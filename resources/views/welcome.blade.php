0b428bc971e295f4606909b4e2d0243878923bc2
Simran <ecare278@gmail.com>
2022-04-14 11:24:06 +0530

    Welcome page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
 
 <div id="content">
-            <h2>About Club</h2>
+            <h2>New About Club</h2>
             <div class="line"></div>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
